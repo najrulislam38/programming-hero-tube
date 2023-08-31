862c96d697a01188f93143015f980f238831c9f3
Najrul <mdnajrulislam.4738@gmail.com>
2023-08-31 22:46:20 +0600

    Added tab section with function in loadpage

--- js/p-hero.js
+++ js/p-hero.js
@@ -3,6 +3,15 @@ const loadPage = async() => {
     const data = await res.json();
 
     console.log(data.data);
+
+    const headerContainer = document.getElementById('tab-container');
+    data.data.forEach((category) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <button class="btn text-black bg-gray-300  text-lg font-medium focus:bg-red-500 focus:text-white">${category.category}</a></button>
+        `;
+        headerContainer.appendChild(div)
+    })
 }
 
 loadPage()
