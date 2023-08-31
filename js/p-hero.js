a23aa59b23663dab842a8dd3fe40f8c0faaa335f
Najrul <mdnajrulislam.4738@gmail.com>
2023-08-31 22:13:02 +0600

    created p-hero.js files and fetch json file

---
+++ js/p-hero.js
@@ -0,0 +1,8 @@
+const loadPage = async() => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+
+    console.log(data.data);
+}
+
+loadPage()
